8471c255725bc7674d810fc6ba28a0c572b97fe1
youssef-erremili <yousseferremili19@gmail.com>
2025-02-02 01:01:32 +0100

    Release beta version from laravel-alert

--- resources/views/alerts/success.blade.php
+++ resources/views/alerts/success.blade.php
@@ -1,13 +1,24 @@
-<div class="animate flex items-center py-4 px-4 max-w-[28rem] fixed top-4 z-[999] right-5 text-sm border-l-4 rounded shadow-sm bg-green-50 border-green-500">
-    <span class="inline-block mx-2">
-        <svg class="h-6 w-6 text-green-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
-    </span>
-    <p class="text-green-500 font-normal text-base text-pretty leading-relaxed capitalize mr-6">{{ $message }}</p>
-    <button class="hide ml-4 inline-flex rounded-md text-slate-400 hover:text-slate-500 focus:outline-none">
-        <svg class="h-5 w-5" viewBox="0 0 20 20" fill="#16a34a" aria-hidden="true">
-            <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
-        </svg>
-    </button>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('vendor/errehub/laravel-alert/css/alert.css') }}?v={{ time() }}">
+</head>
+<body>
+    @if (session('alert_message'))
+        <div class="alert alert-success hide">
+            <span>
+                <svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+            </span>
+            <p>{{ session('alert_message') }}</p>
+        </div> 
+    @endif
+
+    <script src="{{ asset('vendor/errehub/laravel-alert/js/alert.js') }}?v={{ time() }}"></script>
+</body>
+</html>
+
